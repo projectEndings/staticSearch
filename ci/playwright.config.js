690a299d6776333910c33eb5202edcc9d20ae819
Joey Takeda <joeytakeda@users.noreply.github.com>
2024-03-14 19:41:22 -0700

    Adjusting config

--- ci/playwright.config.js
+++ ci/playwright.config.js
@@ -4,8 +4,8 @@ import { defineConfig, devices } from "@playwright/test";
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: "./tests",
-  outputDir: "./results",
+  testDir: "tests",
+  outputDir: "test-results",
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: true,
   retries: 2,
@@ -20,7 +20,7 @@ module.exports = defineConfig({
     baseURL: "http://127.0.0.1:8080",
   },
   webServer: {
-    command: "npx http-server ../",
+    command: "npx -y http-server ../",
     url: "http://127.0.0.1:8080",
     reuseExistingServer: false,
   },
